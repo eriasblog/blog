645d75b04d74b5bcf114bc9c06c1d3c375093296
erias04 <github.eliascerne@icloud.com>
2022-03-30 00:11:09 +0200

    feat: styled the search button

--- libs/util/search/button/src/lib/UtilSearchButton.tsx
+++ libs/util/search/button/src/lib/UtilSearchButton.tsx
@@ -1,13 +1,33 @@
-import styles from './UtilSearchButton.module.css';
+import { useState } from 'react';
+import { FiSearch, FiCommand } from 'react-icons/fi';
+
+import './UtilSearchButton.module.css';
 
 /* eslint-disable-next-line */
 export interface UtilSearchButtonProps {}
 
 export function UtilSearchButton(props: UtilSearchButtonProps) {
   return (
-    <div className={styles['container']}>
-      <h1>Welcome to UtilSearchButton!</h1>
-    </div>
+    <button
+      // onClick={toggleModals}
+      className="bg-searchBarBg border-searchBarBorder border-2 pl-4 py-1 pr-2 rounded-xl cursor-pointer w-full"
+    >
+      <div className="flex justify-between text-searchBarText">
+        <div className="flex items-center gap-2">
+          <FiSearch className="text-xl" />
+          <p className="font-medium text-lg">Search</p>
+        </div>
+        <div className="flex items-center gap-1 text-sm">
+          <kbd className="self-center rounded-md border-searchBarBorder border-2 px-1 py-0.5">
+            <FiCommand />
+          </kbd>
+          <kbd className=" text-center rounded-md border-searchBarBorder border-2 px-1.5 h-[22px]">
+            <p className="-translate-y-[1px]">K</p>
+            
+          </kbd>
+        </div>
+      </div>
+    </button>
   );
 }
 
