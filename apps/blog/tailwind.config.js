5593f54a4415da8fba84f7fd089ed4a81f624a12
erias04 <github.eliascerne@icloud.com>
2022-03-29 22:48:52 +0200

    feat: changed tailwind config file The librarys were not styled so I made my own style implementation (theme) and added a new content join path

--- apps/blog/tailwind.config.js
+++ apps/blog/tailwind.config.js
@@ -1,3 +1,4 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
 const { createGlobPatternsForDependencies } = require('@nrwl/react/tailwind');
 const { join } = require('path');
 
@@ -5,10 +6,44 @@ module.exports = {
   mode: 'jit',
   content: [
     join(__dirname, 'pages/**/!(*.stories|*.spec).{ts,tsx,html}'),
+    join('libs/post/**/!(*.stories|*.spec).{ts,tsx,html}'),
     ...createGlobPatternsForDependencies(__dirname),
   ],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+    },
+    extend: {
+      fontFamily: {
+        sans: ['Poppins', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        white: '#ffffff',
+        primaryColor: '#050F1E',
+        countryCode: '#989DA3',
+        titleColor: '#f1f2f3',
+        searchBarBg: '#1C2534',
+        searchBarBorder: '#27303E',
+        searchBarText: '#8494AA',
+      },
+      maxWidth: {
+        header: '1200px',
+      },
+      height: {
+        header: '3rem',
+      },
+      width: {
+        7.5: '1.8rem',
+      },
+      fontSize: {
+        xxs: '.6rem',
+      },
+      dropShadow: {
+        '3xl': '0 -3px 10px rgba(0, 0, 0, 0.65)',
+      },
+    },
   },
   plugins: [],
 };
