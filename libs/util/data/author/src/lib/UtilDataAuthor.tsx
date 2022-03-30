4d3721886a912d186adf6d81d68f25219b4dfbd8
erias04 <github.eliascerne@icloud.com>
2022-03-30 20:10:33 +0200

    feat: added a Image renderer lob

--- libs/util/data/author/src/lib/UtilDataAuthor.tsx
+++ libs/util/data/author/src/lib/UtilDataAuthor.tsx
@@ -1,13 +1,37 @@
+import Image from 'next/image';
+
 import styles from './UtilDataAuthor.module.css';
 
 /* eslint-disable-next-line */
-export interface UtilDataAuthorProps {}
+export interface UtilDataAuthorProps {
+  width: number;
+  height: number;
+}
 
 export function UtilDataAuthor(props: UtilDataAuthorProps) {
+  const { width, height } = props;
+
+  const myLoader = ({
+    src,
+    width,
+    quality,
+  }: {
+    src: string;
+    width: number;
+    quality?: number;
+  }) => {
+    return `https://yt3.ggpht.com/yti/APfAmoHXagPJqsoblSlAwzkf_We4PmNAVy51AF_OLQyiFw=s${width}-c-k-c0x00ffffff-no-rj-mo`;
+  };
+
   return (
-    <div className={styles['container']}>
-      <h1>Welcome to UtilDataAuthor!</h1>
-    </div>
+    <Image
+      loader={myLoader}
+      className="rounded-full"
+      src="unnamed.png"
+      alt="Picture of the author"
+      width={width}
+      height={height}
+    />
   );
 }
 
