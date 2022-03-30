8eb31fbd363637e3420e54f2513a1f2b290f369d
erias04 <github.eliascerne@icloud.com>
2022-03-30 23:35:40 +0200

    feat: styled post page

--- libs/post/main/ui/src/lib/PostMainUi.tsx
+++ libs/post/main/ui/src/lib/PostMainUi.tsx
@@ -1,3 +1,8 @@
+import { UtilDataAuthor } from '@blog/util/data/author';
+import { BsThreeDots, BsBookmarkPlus } from 'react-icons/bs';
+import { FaTwitter, FaFacebook, FaLinkedin } from 'react-icons/fa';
+import { AiOutlineLink } from 'react-icons/ai';
+
 import styles from './PostMainUi.module.css';
 
 /* eslint-disable-next-line */
@@ -5,9 +10,27 @@ export interface PostMainUiProps {}
 
 export function PostMainUi(props: PostMainUiProps) {
   return (
-    <div className={styles['container']}>
-      <h1>Welcome to PostMainUi!</h1>
-    </div>
+    <main className="container mt-4">
+      <div className="flex gap-2 mb-4 ml-3 w-100 items-center">
+        <UtilDataAuthor className="rounded-full" width={44} height={44} />
+        <div className="grid gap-1">
+          <div className="flex gap-2">
+            <p className="text-[15px] font-semibold">Elias Cerne</p>
+            <button className="bg-[#047AFF] px-2 py-0.5 rounded-xl text-white text-xs">
+              Follow
+            </button>
+          </div>
+          <div className="flex gap-1 text-[13px] text-[#676767] font-medium">
+            <p>Mar 23 2022</p>
+            <p>•</p>
+            <p>5 min read</p>
+          </div>
+        </div>
+        <span className="flex grow justify-end mr-4">
+          <BsThreeDots className="text-xl" />
+        </span>
+      </div>
+    </main>
   );
 }
 
